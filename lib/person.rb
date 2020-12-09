32b53ab0dc39e28c626d63dbcf16f59a033b77c8
“Abbie <abbie.coghlan@gmail.com>
2020-12-09 11:39:20 -0600

    commit tree 4ede3d5b4beb9e8522fc8cd9e6c2f9a9fc6351b5

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+    #getter
+    def name
+        @name
+    end
+
+    #setter
+    def name=(name)
+        @name = name
+    end
+
+    #getter
+    def job
+        @job
+    end
+
+    #setter
+    def job=(job)
+        @job = job
+    end
+
+end
